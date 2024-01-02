9f3e0ab49aaf7f09b9bc78d7721a5ac2ebb41b06
damiankostycz <damian.kostycz@gmail.com>
2024-01-02 13:00:19 +0100

    create spring project

---
+++ src/test/java/bus/project/oauth_resourceserver/OAuthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bus.project.oauth_resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
