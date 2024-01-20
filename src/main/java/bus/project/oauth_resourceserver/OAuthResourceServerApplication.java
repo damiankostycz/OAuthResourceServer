35ca72c8abb12ef452e1a09c504fe10921a012d6
mchomick <01169272@pw.edu.pl>
2024-01-20 18:23:59 +0100

    :) v4

--- src/main/java/bus/project/oauth_resourceserver/OAuthResourceServerApplication.java
+++ src/main/java/bus/project/oauth_resourceserver/OAuthResourceServerApplication.java
@@ -1,16 +1,22 @@
 package bus.project.oauth_resourceserver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableMongoRepositories
-
+@EnableMethodSecurity
 public class OAuthResourceServerApplication {
 
 
@@ -19,6 +25,30 @@ public class OAuthResourceServerApplication {
 
     }
 
+}
 
+@Service
+class GreetingsService {
 
+    @PreAuthorize("hasAuthority('SCOPE_user.read')")
+    public Map<String, String> greet() {
+        var jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return Map.of("message", "Hello " + jwt.getSubject());
+    }
+}
+
+@Controller
+@ResponseBody
+class GreetingsController {
+
+    private final GreetingsService service;
+
+    GreetingsController(GreetingsService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    Map<String, String> hello() {
+        return this.service.greet();
+    }
 }
--- src/main/java/bus/project/oauth_resourceserver/OAuthResourceServerController.java
+++ src/main/java/bus/project/oauth_resourceserver/OAuthResourceServerController.java
@@ -5,26 +5,26 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
-public class OAuthResourceServerController {
-
-    @Autowired
-    DataService dataService;
-    @GetMapping("/all-clients")
-    public String getSecuredData() {
-        return this.dataService.findAllClients().toString() ;
-    }
-    @GetMapping("/get-clients-by-name")
-    public String getClientByName(@RequestParam String firstName) {
-        return this.dataService.findItemByName(firstName).toString() ;
-    }
-    @GetMapping("/")
-    public String index(@AuthenticationPrincipal Jwt jwt) {
-        return String.format("Hello, %s!", jwt.getSubject());
-    }
-
-
-
-
-}
+//@Controller
+//@ResponseBody
+//public class OAuthResourceServerController {
+//
+//    @Autowired
+//    DataService dataService;
+//    @GetMapping("/all-clients")
+//    public String getSecuredData() {
+//        return this.dataService.findAllClients().toString() ;
+//    }
+//    @GetMapping("/get-clients-by-name")
+//    public String getClientByName(@RequestParam String firstName) {
+//        return this.dataService.findItemByName(firstName).toString() ;
+//    }
+//    @GetMapping("/")
+//    public String index(@AuthenticationPrincipal Jwt jwt) {
+//        return String.format("Hello, %s!", jwt.getSubject());
+//    }
+//
+//
+//
+//
+//}
